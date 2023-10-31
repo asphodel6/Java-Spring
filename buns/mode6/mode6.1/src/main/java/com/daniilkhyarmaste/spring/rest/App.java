67bc4bb79516b1b7900c3697b1a928991a76a1e4
asphodel6 <harmsss@mail.ru>
2023-10-31 23:44:43 +0500

    mode6 push

---
+++ buns/mode6/mode6.1/src/main/java/com/daniilkhyarmaste/spring/rest/App.java
@@ -0,0 +1,30 @@
+package com.daniilkhyarmaste.spring.rest;
+
+import com.daniilkhyarmaste.spring.rest.configuration.MyConfig;
+import com.daniilkhyarmaste.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = context.getBean("communication", Communication.class);
+        List<Employee> allEmployees = communication.getAllEmployees();
+        System.out.println(allEmployees);
+
+        Employee empByID = communication.getEmployee(1);
+        System.out.println(empByID);
+
+        Employee emp = new Employee("Sveta", "Sokolova", "HR", 900);
+        communication.saveEmployee(emp);
+
+        communication.deleteEmployee(2);
+    }
+}
