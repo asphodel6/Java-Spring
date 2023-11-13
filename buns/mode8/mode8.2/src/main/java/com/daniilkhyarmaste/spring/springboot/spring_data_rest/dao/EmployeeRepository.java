b945c155f836175439b8b211d80c1db7acee84fb
asphodel6 <harmsss@mail.ru>
2023-11-14 00:14:41 +0500

    mode8 push

---
+++ buns/mode8/mode8.2/src/main/java/com/daniilkhyarmaste/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.daniilkhyarmaste.spring.springboot.spring_data_rest.dao;
+
+import com.daniilkhyarmaste.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
---
+++ buns/mode8/src/main/java/com/daniilkhyarmaste/spring/springboot/spring_course_springboot/Mode8Application.java
@@ -0,0 +1,13 @@
+package com.daniilkhyarmaste.spring.springboot.spring_course_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mode8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mode8Application.class, args);
+	}
+
+}
